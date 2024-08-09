ac288c9e556f441d193ced2cfbf90b9da03ddb4e
W. Shashindu Chamika <126416494+WShashinduChamika@users.noreply.github.com>
2024-08-09 10:37:27 +0530

    Updated notification screen #65

--- sn_meals/lib/screens/more/notification_screen.dart
+++ sn_meals/lib/screens/more/notification_screen.dart
@@ -34,109 +34,125 @@ class _NotificationScreenState extends State<NotificationScreen> {
     double width = MediaQuery.of(context).size.width / 375;
     double height = MediaQuery.of(context).size.height / 800;
 
-    return Scaffold(
-     
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 12,
-              vertical: height * 20,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Notifications',
-                  style: TextStyle(
-                    fontSize: width * 22,
-                    fontWeight: FontWeight.w600,
-                    color: TColor.primaryText,
-                  ),
-                ),
-                Image.asset(
-                  'assets/img/more/notifications.png',
-                  height: height * 20,
-                  width: width * 20,
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                  vertical: height * 0,
-                ),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: height * 0,
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: height * 20,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Image.asset(
+                      'assets/img/more/back.png',
+                      height: height * 25,
+                      width: width * 25,
+                      color: Colors.black,
                     ),
-                    ListView.builder(
-                      padding: EdgeInsets.symmetric(
-                          vertical: height * 30, horizontal: width * 20),
-                      physics: const NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: notificationsArr.length,
-                      itemBuilder: (context, index) {
-                        var notiObj = notificationsArr[index] as Map? ?? {};
-                        return GestureDetector(
-                          onTap: () {
-                           
-                          },
-                          child: Container(
-                            padding: EdgeInsets.symmetric(
-                              horizontal: width * 10,
-                              vertical: height * 20,
-                            ),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Container(
-                                  height: height * 10,
-                                  width: width * 10,
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: TColor.primary,
+                  ),
+                  SizedBox(
+                    width: width * 20,
+                  ),
+                  Text(
+                    'Notifications',
+                    style: TextStyle(
+                      fontSize: width * 22,
+                      fontWeight: FontWeight.w600,
+                      color: TColor.primaryText,
+                    ),
+                  ),
+                  SizedBox(
+                    width: width * 100,
+                  ),
+                  Expanded(
+                    child: Image.asset(
+                      'assets/img/more/cart.png',
+                      height: height * 20,
+                      width: width * 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: height * 10,
+                  ),
+                  child: Column(
+                    children: [
+                      ListView.builder(
+                        padding: EdgeInsets.symmetric(
+                            vertical: height * 0, horizontal: width * 20),
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: notificationsArr.length,
+                        itemBuilder: (context, index) {
+                          var notiObj = notificationsArr[index] as Map? ?? {};
+                          return GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: width * 10,
+                                vertical: height * 10,
+                              ),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    height: height * 10,
+                                    width: width * 10,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: TColor.primary,
+                                    ),
                                   ),
-                                ),
-                                SizedBox(
-                                  width: width * 20,
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      notiObj["name"],
-                                      style: TextStyle(
-                                        color: TColor.primaryText,
-                                        fontSize: width*14,
-                                        fontWeight: FontWeight.w500,
+                                  SizedBox(
+                                    width: width * 20,
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        notiObj["name"],
+                                        style: TextStyle(
+                                          color: TColor.primaryText,
+                                          fontSize: width * 14,
+                                          fontWeight: FontWeight.w500,
+                                        ),
                                       ),
-                                    ),
-                                    Text(
-                                      notiObj["time"],
-                                      style: TextStyle(
-                                        color: TColor.placeholder,
-                                        fontSize: width*12,
-                                        fontWeight: FontWeight.w500,
+                                      Text(
+                                        notiObj["time"],
+                                        style: TextStyle(
+                                          color: TColor.placeholder,
+                                          fontSize: width * 12,
+                                          fontWeight: FontWeight.w500,
+                                        ),
                                       ),
-                                    ),
-                                  ],
-                                ),
-                              ],
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        );
-                      },
-                    )
-                  ],
+                          );
+                        },
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
